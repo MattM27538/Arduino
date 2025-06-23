b16c12c17afe9e3cd1caba42ae622abea796c0fd
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-06-23 16:20:03 -0700

    Remove parameter and arugments for changeLEDColor function

--- RGBLED.ino
+++ RGBLED.ino
@@ -11,7 +11,7 @@ namespace {
   int blueValue{0};
 }
 
-void changeLEDColor(int& currColorValue,int& newColorValue,int& thirdColorValue,uint8_t currPinColor,uint8_t newPinColor, const int delayTime){
+void changeLEDColor(int& currColorValue,int& newColorValue,uint8_t currPinColor,uint8_t newPinColor, const int delayTime){
   for(int i{0};i<255;++i){
     --currColorValue;
     ++newColorValue;
@@ -38,12 +38,12 @@ void loop(){
   constexpr unsigned long delayTime{10}; //Transition time between LED color fades.
   
   //Change LED from red to green.
-  changeLEDColor(::redValue,::greenValue,::blueValue,static_cast<uint8_t>(pinColor::RED),static_cast<uint8_t>(pinColor::GREEN),delayTime);
+  changeLEDColor(::redValue,::greenValue,static_cast<uint8_t>(pinColor::RED),static_cast<uint8_t>(pinColor::GREEN),delayTime);
 
   //Change LED from green to blue.
-  changeLEDColor(::greenValue,::blueValue,::redValue,static_cast<uint8_t>(pinColor::GREEN),static_cast<uint8_t>(pinColor::BLUE),delayTime);
+  changeLEDColor(::greenValue,::blueValue,static_cast<uint8_t>(pinColor::GREEN),static_cast<uint8_t>(pinColor::BLUE),delayTime);
 
   //Change LED from blue to red.
-  changeLEDColor(::blueValue,::redValue,::greenValue,static_cast<uint8_t>(pinColor::BLUE),static_cast<uint8_t>(pinColor::RED),delayTime);
+  changeLEDColor(::blueValue,::redValue,static_cast<uint8_t>(pinColor::BLUE),static_cast<uint8_t>(pinColor::RED),delayTime);
 }
 
