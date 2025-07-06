d40e4a3c6c07a9f895dbaa5f186767f16c9ee43b
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-07-05 17:59:32 -0700

    Initial push for buzzer heartbeat simulator code

---
+++ ActiveBuzzer/ActiveBuzzer.ino
@@ -0,0 +1,42 @@
+//Sketch activates active buzzer to simulate heartbeat monitor.
+
+namespace{
+  namespace Buzzer{
+    uint8_t pin{12};
+    unsigned long soundDuration{500};
+  }
+}
+
+void setup(){
+  pinMode(Buzzer::pin, OUTPUT); //Initialize the buzzer pin as an output.
+}
+
+void loop(){
+  for(int i{0}; i<20; ++i) {
+    //Adjust buzzer duration time.
+    if(i<5){
+      Buzzer::soundDuration=500;
+    } 
+    else if(i<10){
+      Buzzer::soundDuration=300;
+    } 
+    else if(i<20){
+      Buzzer::soundDuration=100;
+    }
+
+    //Activate the buzzer.
+    digitalWrite(Buzzer::pin, HIGH);
+
+    delay(Buzzer::soundDuration);
+
+    //Deactivate the buzzer.
+    digitalWrite(Buzzer::pin, LOW);
+
+    delay(Buzzer::soundDuration);
+  }
+  
+  //Activate the buzzer.
+  digitalWrite(Buzzer::pin, HIGH);
+
+  delay(5000);//keep playing sound for 5 seconds.
+}
