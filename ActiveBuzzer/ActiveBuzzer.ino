9a50494fe99aee3aec093fe356bb04881325e264
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-07-06 11:53:44 -0700

    Add scope resolution operator for unnamed namespace access

--- ActiveBuzzer/ActiveBuzzer.ino
+++ ActiveBuzzer/ActiveBuzzer.ino
@@ -8,35 +8,35 @@ namespace{
 }
 
 void setup(){
-  pinMode(Buzzer::pin, OUTPUT); //Initialize the buzzer pin as an output.
+  pinMode(::Buzzer::pin, OUTPUT); //Initialize the buzzer pin as an output.
 }
 
 void loop(){
   for(int i{0}; i<20; ++i) {
     //Adjust buzzer duration time.
     if(i<5){
-      Buzzer::soundDuration=500;
+      ::Buzzer::soundDuration=500;
     } 
     else if(i<10){
-      Buzzer::soundDuration=300;
+      ::Buzzer::soundDuration=300;
     } 
     else if(i<20){
-      Buzzer::soundDuration=100;
+      ::Buzzer::soundDuration=100;
     }
 
     //Activate the buzzer.
-    digitalWrite(Buzzer::pin, HIGH);
+    digitalWrite(::Buzzer::pin, HIGH);
 
-    delay(Buzzer::soundDuration);
+    delay(::Buzzer::soundDuration);
 
     //Deactivate the buzzer.
-    digitalWrite(Buzzer::pin, LOW);
+    digitalWrite(::Buzzer::pin, LOW);
 
-    delay(Buzzer::soundDuration);
+    delay(::Buzzer::soundDuration);
   }
   
   //Activate the buzzer.
-  digitalWrite(Buzzer::pin, HIGH);
+  digitalWrite(::Buzzer::pin, HIGH);
 
   delay(5000);//keep playing sound for 5 seconds.
 }
